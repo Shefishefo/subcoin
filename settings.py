f9096f8e9bec7e3ffd9bc1992dff65613fb9271f
Shefishefo <86641801+Shefishefo@users.noreply.github.com>
2023-03-30 07:52:47 +0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,20 @@
+
+token = "6176732238:AAHs6DJJJTFNss7jB6nKSZWP7ocx1RpjRUM"
+
+botname = 'Coin_extremebot'
+
+admins = [5155125092] #админы бота через запятую
+
+LITTLE_SUBCOIN_TO_GET_SUBS = 2 #минимум сабкоинов для раскрутки канала
+
+ref_count = 2 #реферальный бонус (сабкоины)
+
+REF_BONUS = 2
+
+FINE_FOR_UNSUBSCRIBING = -7 #Число должно быть отрицательным и целым
+
+SUBSCRIPTION_TERM = 7 #Какое время юзер должен оьязательно быть подписан на канал?
+
+LINK_TO_INTRODUCTION_AND_RULES = 'https://telegra.ph/Vvedenie-v-bota-a-takzhe-pravila-06-09' #ссылка на правила и инструкцию
+
+LINK_TO_CHAT_OF_BOT = '@gamer_zoneee' #ссылка на чат
